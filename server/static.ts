9777d91f0a361752bbb55baf678bd52caa148cbd
urbanpoona2025 <urbanpoona2025@gmail.com>
2026-03-20 21:39:58 +0530

    claude fix one

--- server/static.ts
+++ server/static.ts
@@ -1,4 +1,4 @@
-import express, { type Express } from "express";
+import express, { type Express, Request, Response } from "express";
 import fs from "fs";
 import path from "path";
 
@@ -13,7 +13,7 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("/{*path}", (_req, res) => {
+app.use("/{*path}", (_req: Request, res: Response) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
