52d4d3fc1a76fe96809c7c40a83776dcbf9757af
urbanpoona2025 <urbanpoona2025@gmail.com>
2026-03-20 20:59:49 +0530

    final try

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,29 @@
-import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
-import { serveStatic } from "./static";
-import { createServer } from "http";
-
-const app = express();
-const httpServer = createServer(app);
-
 declare module "http" {
   interface IncomingMessage {
     rawBody: unknown;
   }
 }
+
+import express, { type Request, Response, NextFunction } from "express";
+import { registerRoutes } from "./routes";
+import { serveStatic } from "./static";
+import { createServer } from "http";
+
+export function log(message: string, source = "express") {
+  const formattedTime = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+
+  console.log(`[${formattedTime}] [${source}] ${message}`);
+}
+
+
+(async () => {
+const app = express();
+const httpServer = createServer(app);
 
 app.use(
   express.json({
@@ -21,27 +34,17 @@ app.use(
 );
 
 app.use(express.urlencoded({ extended: false }));
-
-export function log(message: string, source = "express") {
-  const formattedTime = new Date().toLocaleTimeString("en-US", {
-    hour: "numeric",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: true,
-  });
-
-  console.log(`${formattedTime} [${source}] ${message}`);
 }
 
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   const start = Date.now();
   const path = req.path;
   let capturedJsonResponse: Record<string, any> | undefined = undefined;
 
   const originalResJson = res.json;
-  res.json = function (bodyJson, ...args) {
+  res.json = function (bodyJson: any, ...args: any[]) {
     capturedJsonResponse = bodyJson;
-    return originalResJson.apply(res, [bodyJson, ...args]);
+    return originalResJson.apply(res, [bodyJson: any, ...args]);
   };
 
   res.on("finish", () => {
@@ -59,7 +62,7 @@ app.use((req, res, next) => {
   next();
 });
 
- registerRoutes(httpServer, app);
+await registerRoutes(httpServer as any, app);
 
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
@@ -77,13 +80,9 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if(process.env.NODE_ENV === "production") {
-  serveStatic(app);
-} else {
-  import("./vite").then(({ setupVite }) => {
-    setupVite(httpServer, app);
-  });
-}
+  if (process.env.NODE_ENV === "production") {
+  serveStatic(app as any);
+  }
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
@@ -92,4 +91,6 @@ app.use((req, res, next) => {
 
 httpServer.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
-});
+  });
+
+})();
